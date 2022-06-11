0ad50ed9f3c12a826f54fb113993e87f65d2afb9
kirill.novgorodtsev <cmyser.fast@mail.ru>
2022-06-11 11:02:16 +0300

    copilot

--- start.py
+++ start.py
@@ -1,1 +1,27 @@
 input("Learn_python" + " " + "Press Enter to continue...")
+
+
+#   этот код полностью написан с помощью github copilot!!!
+# write a program that asks for two numbers and prints the bigger one (without using if)
+def bigger_number(a, b):
+    if a > b:
+        return a
+    else:
+        return b
+    
+# write a function that takes a list of numbers and returns the sum of the biggest and the smallest number
+def sum_biggest_and_smallest(numbers):
+    for number in numbers:
+        if number > numbers[0]:
+            numbers[0] = number
+    print(numbers[0])
+    
+    for number in numbers:
+        if number < numbers[1]:
+            numbers[1] = number
+    print(numbers[1])
+    
+    return numbers[0] + numbers[1]
+
+
+print(sum_biggest_and_smallest(numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
